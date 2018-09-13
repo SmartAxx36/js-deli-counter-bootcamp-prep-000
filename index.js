9d0e6d12d72393fd11fc0f926f3afaf923116e5a
Gavin Moore <gavizoid16@gmail.com>
2018-09-13 19:27:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing(katzDeliLine) {
  }}
  function currentLine(line) {
   let deliLine = []
-  for (let i=0; i < line.length; i++) {
+  for (let i=0; i < line.length-1; i++) {
     deliLine.push(i+". "+line[i])
    }
    let i=0
