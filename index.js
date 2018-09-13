d064f0bd446a3539515c22b79b85c908e5728801
Gavin Moore <gavizoid16@gmail.com>
2018-09-13 18:22:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function nowServing(katzDeliLine) {
   let i = 0
  if (i < katzDeliLine.length) {
    katzDeliLine.shift(name)
-   return ("Currently serving "+(katzDeliLine.length-1)+".")
+   return ("Currently serving "+(katzDeliLine[1])+".")
  } else {
    return ("There is nobody waiting to be served!")
  }
