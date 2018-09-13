3e99d16647e5b639aeb6d4d4ceb6dc37bf63c4d6
Gavin Moore <gavizoid16@gmail.com>
2018-09-13 17:53:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function takeANumber(katzDeli, name) {
   return ("Welcome, "+ name +". You are number "+(katzDeli.length)+" in line.")
 }
 function nowServing(katzDeliLine) {
-  
+  katzDeliLine.shift(name)
   return 
 }
