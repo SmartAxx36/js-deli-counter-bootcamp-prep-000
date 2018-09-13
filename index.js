dbd95110db18024299bffdce49f3f51994a0f2d4
Gavin Moore <gavizoid16@gmail.com>
2018-09-13 18:17:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber(katzDeli, name) {
   return ("Welcome, "+ name +". You are number "+(katzDeli.length)+" in line.")
 }
 function nowServing(katzDeliLine) {
- if () {
+ if (1 < 2) {
    return ("Currently serving "+(katzDeliLine.shift(name))+".")
  } else {
    return ("There is nobody waiting to be served!")
