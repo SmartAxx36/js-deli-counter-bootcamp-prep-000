d6386a04dd24ac9d49367823969c6f1f2f2e37e7
Gavin Moore <gavizoid16@gmail.com>
2018-09-13 18:49:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing(katzDeliLine) {
    return ("There is nobody waiting to be served!")
  }}
  function currentLine(line) {
-   for (i=0)
+   for (i=0, i < line.length)
    if (i < line.length) {
     return ("The line is currently:"+line.length+". "+line)
    } else {
